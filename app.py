f468e86c90f1640daa00031d365e1146f3b0d159
Aishahassan1 <aishahassan_5@hotmail.com>
2022-03-05 11:42:36 +0000

    add virtual evn

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # import
+# dcc = dash core components - the interactive elements such as drop downs
 import petl as ptl
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
-import plotly.express as ex
+import plotly.express as px
 import pandas as pd
 
 # setup
-app =  dash.Dash(__name__, title='Dash Deployment')
+app =  dash.Dash(__name__, title='Dash App')
 server = app.server
 
 # initialize
@@ -15,11 +16,15 @@ app = dash.Dash()
 
 # layout
 app.layout = html.Div([
-    html.H1('Sales Data, 2010-2020', className='header'),
+    html.P('wooohoooo it is working')
 ])
 
 # callbacks
-
+app.callback (
+    # output
+    # input
+    # state
+)
 
 # running
 if __name__ == '__main__':
